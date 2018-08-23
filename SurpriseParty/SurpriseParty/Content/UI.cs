87015dd33f70a434175d9506a339a72ad057a95a
HUANG Bocheng <a923000213@163.com>
2018-08-23 12:42:11 -0600

    UI Update

--- SurpriseParty/SurpriseParty/Content/UI.cs
+++ SurpriseParty/SurpriseParty/Content/UI.cs
@@ -10,19 +10,23 @@ namespace SurpriseParty.Content
 {
     class UI:Component
     {
-
+        private int UIMoveSpeed = 2;
         private Texture2D _texture;
         private UI _parent;
+        private List<UI> _children;
         private Rectangle _rectangle;
         private Point _centerPoint;
-
+     private   bool moving = false;
+        private Point _destination;
         public Rectangle Rectangle { get { return _rectangle; } }
 
         public float alpha;
 
         public UI (UI parent, Texture2D texture, Point centerPoint)
         {
+            _children = new List<UI>();
             _parent = parent;
+            
             _texture = texture;
             _rectangle = new Rectangle(_centerPoint.X - (int)(_texture.Width / 2), _centerPoint.Y - (int)(_rectangle.Height / 2), (_texture.Width / 2), (_texture.Height / 2));
         }
@@ -34,7 +38,28 @@ namespace SurpriseParty.Content
 
         public override void Update(GameTime gameTime)
         {
-            
+            if (moving)
+            {
+
+                
+
+                if(_centerPoint.X == _destination.X && _centerPoint.Y == _destination.Y)
+                {
+                    // moving done
+                    moving = false;
+                }
+            }
+        }
+
+        public void AddChildUI(UI ui)
+        {
+            _children.Add(ui);
+        }
+
+        public void MoveTo(Point direction)
+        {
+            moving = true;
+            _destination = _centerPoint + direction;
         }
     }
 }
